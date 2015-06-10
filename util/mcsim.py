58c0665c6a32a5f922237982dda83e8ddc75b6b6
Francis <fpieraut@gmail.com>
2015-06-10 00:43:41 -0400

    initial monte carlo simumation

--- util/mcsim.py
+++ util/mcsim.py
@@ -5,7 +5,7 @@ from random import random
 # based on https://www.youtube.com/watch?v=e79OtCamxD0
 # ln(S/S-1)= drift + rand * vol
 # drift = expected return for the stock
-def sim(n=10000, annual_drift=.1, annual_volatility=.4, init_price=100, verbose=True):
+def walk(n=1000, annual_drift=.1, annual_volatility=.4, init_price=100, verbose=True):
     drift = float(annual_drift)/252
     vol = float(annual_volatility)/np.sqrt(252)
     drift_mean = drift-0.5*math.pow(vol,2)
@@ -19,9 +19,11 @@ def sim(n=10000, annual_drift=.1, annual_volatility=.4, init_price=100, verbose=
         z = ndtri(r)
         log_return = drift+vol*z
         price = previous_price*math.exp(log_return)
-        print "#%i r=%.2f z=%.2f ret=%.2f%% price=%.2f$" %(i, r, z, log_return*100, price)
+        if verbose:
+            print "#%i r=%.2f z=%.2f ret=%.2f%% price=%.2f$" %(i, r, z, log_return*100, price)
         previous_price = price
     
+    
 if __name__=="__main__":
-    sim()
+    walk()
     
