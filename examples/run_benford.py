c3a2f251721799b353baf1a88d219fcfa6f96996
osboxes.org <osboxes@osboxes.(none)>
2015-06-10 05:13:05 +0100

    Benford exmple now iterates over list of financial doc data

--- examples/run_benford.py
+++ examples/run_benford.py
@@ -8,15 +8,16 @@ stock = 'GOGO'
 data = edgar.get_financial_docs(stock)
 values = []
 
-for item in data['result']['rows'][0]['values']:
-    try:
-	if item['value'] is not None:
-            values.append(int(item['value']))
-    except ValueError:
-        pass
+for sheet in data['result']['rows']:
+    for item in sheet['values']:
+        try:
+	    if item['value'] is not None:
+                values.append(int(item['value']))
+        except ValueError:
+            pass
 
-benford_law   = pyb.benford_law()
-benford_stock = pyb.calc_firstdigit(values[12:])
+    benford_law   = pyb.benford_law()
+    benford_stock = pyb.calc_firstdigit(values[12:])
 
-pyb.plot_comparative(benford_stock, benford_law, str(stock) + " Financial Statement Numbers ")
+    pyb.plot_comparative(benford_stock, benford_law, str(stock) + " Financial Statement Numbers ")
 
